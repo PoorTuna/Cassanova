779029b818aa01315c7f11375870e3c87acf8ca0
Oren <orengamingil@gmail.com>
2025-06-24 02:57:00 +0300

    API Router
    Added an api router to cassanova which gives an alternative to the UI.

--- cassanova/config/app_config.py
+++ cassanova/config/app_config.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel, Field
 class APPConfig(BaseModel):
     host: str = Field(default='0.0.0.0')
     port: int = Field(default=8080)
-    routers: list[str] = ['cassanova_router']
+    routers: list[str] = ['cassanova_router', 'cassanova_api_router']
--- cassanova/consts/app_routers.py
+++ cassanova/consts/app_routers.py
@@ -1,7 +1,9 @@
+from cassanova.api.routes.cassanova_api_routes import cassanova_api_router
 from cassanova.api.routes.cassanova_routes import cassanova_router
 
 
 class APPConsts:
     AVAILABLE_ROUTER_MAPPING = {
-        'cassanova_router': cassanova_router
+        'cassanova_router': cassanova_router,
+        'cassanova_api_router': cassanova_api_router,
     }
