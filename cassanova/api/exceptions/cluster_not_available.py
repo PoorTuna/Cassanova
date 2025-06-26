eb9f03dd6aa9ab55dcde3b19f0400a6d42c099c7
Oren <orengamingil@gmail.com>
2025-06-26 17:16:31 +0300

    Cluster Down real exception
    Modified the cluster down page to include the actual exception message.

--- cassanova/api/exceptions/cluster_not_available.py
+++ cassanova/api/exceptions/cluster_not_available.py
@@ -11,5 +11,5 @@ templates = Jinja2Templates(directory="web/templates")
 def add_cluster_unavailable_exceptions(app: FastAPI):
     @app.exception_handler(NoHostAvailable)
     async def cluster_unavailable_exception_handler(request: Request, exc: NoHostAvailable):
-        return templates.TemplateResponse("exceptions/cluster-down.html", {"request": request},
+        return templates.TemplateResponse("exceptions/cluster-down.html", {"request": request, 'exception': str(exc)},
                                           status_code=HTTPStatus.SERVICE_UNAVAILABLE)
